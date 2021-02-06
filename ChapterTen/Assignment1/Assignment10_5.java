4dfbec32385c63ad96b1c54d17f9ca83bcc07229
Charlesccharris <harriscc@student.swosu.edu>
2021-02-06 15:20:54 -0600

    Created the StackOfIntegers class from the book

--- ChapterTen/Assignment1/Assignment10_5.java
+++ ChapterTen/Assignment1/Assignment10_5.java
@@ -1,5 +1,21 @@
-public class Assignment10_5{
+class Assignment10_5{
 	public static void main(String[] args){
-		System.out.println("Hello");
+/*		(Displaying the prime factors)
+		Write a program that prompts the user to enter a
+		positive integer and displays all its smallest
+		factors in decreasing order.
+
+		For example;
+		If the integer is 120
+		the smallest factors are displayed as 5, 3, 2, 2, 2
+		Use the StackOfIntegers class to store the factors
+		and retrieve and display them in reverse order
+*/
+
+		int number = 120;
+		StackOfIntegers listOfPrimes = new StackOfIntegers();
+//		System.out.println("Please input your whole number: ");
+
+		
 	}
 }
