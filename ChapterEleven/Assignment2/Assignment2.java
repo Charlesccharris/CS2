3db2cfb0c4105b1e1823702a05939bafdd3b4756
Charlesccharris <harriscc@student.swosu.edu>
2021-02-18 15:16:09 -0600

    Added the instructions for the Assignments

--- ChapterEleven/Assignment2/Assignment2.java
+++ ChapterEleven/Assignment2/Assignment2.java
@@ -1,5 +1,11 @@
 class Assignment2{
 	public static void main(String[] args){
+/*		Write a program that randomly
+		fills in 0s and 1s into an n-by-n matrix.
+
+		Under Programming Exercises 11.9,
+		follow the direction found on page 449 of your text book.
+*/
 		System.out.println("Hello");
 	}
 }
--- ChapterEleven/Assignment3/Assignment3.java
+++ ChapterEleven/Assignment3/Assignment3.java
@@ -1,5 +1,11 @@
 class Assignment3{
 	public static void main(String[] args){
+/*		Write a method that removes
+		the duplicate elements from an array list of integers.
+
+		Under Programming Exercises 11.13,
+		follow the direction found on page 449 of your text book.
+*/
 		System.out.println("Hi");
 	}
 }
