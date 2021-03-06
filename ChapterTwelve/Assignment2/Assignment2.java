2fc5604e9ec8e963ea0f126a6f6aed587dcd2adb
Charlesccharris <harriscc@student.swosu.edu>
2021-03-06 11:46:29 -0600

    Created the basic class, and got the instructions

---
+++ ChapterTwelve/Assignment2/Assignment2.java
@@ -0,0 +1,12 @@
+class Assignment2{
+	public static void main(String[] args){
+/*		(Count characters, words, and lines in a file)
+		Write a program that will count the number of
+		characters, words, and lines in a file.
+		Words are separated by whitespace charactes.
+		The file name should be passed as a command-line
+		argument, as shown in figue 12.13.
+*/
+		System.out.println(99);
+	}
+}
---
+++ ChapterTwelve/Assignment3/Assignment3.java
@@ -0,0 +1,24 @@
+class Assignment3{
+	public static void main(String[] args){
+/*		(Baby name popularity ranking)
+		The popularity ranking of baby names from
+		years 2001 to 2010 is downloaded from
+		www.ssa.gov/oact/babynames
+		and stored in files named
+		babynameranking2001.txt,
+		babynameranking2002.txt, ...,
+		babynameranking2010.txt.
+
+		Each file contains one thousand lines.
+		Each line contains a ranking, a boy's name,
+		number for the boy's name, a girl's name,
+		and number for the girl's name.
+
+		Write a program that prompts the user to
+		enter the year, gender, and followed by a name,
+		and displays the ranking of the name for the year.
+
+*/
+		
+	}
+}
